150c7918a9fb2194e112f3aecb958b1566ce8379
kvanadev <kvanadev1@gmail.com>
2017-01-13 19:49:10 +0530

    secondview is completed

---
+++ screen/seconViewController.swift
@@ -0,0 +1,35 @@
+//
+//  seconViewController.swift
+//  screen
+//
+//  Created by kvanadev5 on 13/01/17.
+//  Copyright © 2017 kvanadev5. All rights reserved.
+//
+
+import UIKit
+
+class seconViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.gduwqudqwhidhiuqwhdbhd
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
